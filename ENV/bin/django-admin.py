31415a8bb7985f93d6f6cd73a98ad0ad0129927c
pratik <nichatpratik@gmail.com>
2019-05-06 14:06:49 -0700

    Developed the skeleton website

---
+++ ENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pratik/Desktop/Library_management_system/Library_management_system/ENV/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
