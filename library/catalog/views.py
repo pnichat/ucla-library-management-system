dcd51c397f1b3cd052d3b86ce21044b41e32df47
pratik <nichatpratik@gmail.com>
2019-05-11 18:02:56 -0700

    User Login and logout included with session features

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -8,20 +8,33 @@ def index(request):
 	num_instances = BookInstance.objects.all().count()
 	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 	num_authors = Author.objects.all().count()
-
+	num_visits = request.session.get('num_visits',0)
+	request.session['num_visits'] = num_visits + 1
 	context = {
 		'num_books': num_books,
 		'num_instances': num_instances,
 		'num_instances_available': num_instances_available,
 		'num_authors': num_authors,
+		'num_visits': num_visits,
 	}
 
 	return render(request, 'index.html', context=context)
 
 from django.views import generic
 
-class BookListView(generic.ListView):
-	model = Book
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def BookList(request):
+	booklist = Book.objects.all()
+	context = {
+	'book_list': booklist,
+	}
+	return render(request, 'catalog/book_list.html', context=context)
+
+
+#class BookListView(generic.ListView):
+#	model = Book
 
 class BookDetailView(generic.DetailView):
 	model = Book
